90d228691a0786553fa7dfaaa87bfbd83f8659d7
胡世豪 <1126006535@qq.com>
2024-07-18 16:28:50 +0800

    vue-初始化项目

---
+++ vue-project/src/MyVue.vue
@@ -0,0 +1,31 @@
+<template>
+    <body>
+        <div>
+            <h1>模板语法</h1>
+            <p>{{ msg }}</p>
+            <p>{{ date }}</p>
+            <p>{{ isTrue ? msg: data }}</p>
+        
+        </div>
+        <div v-html="buttonHtml">
+        </div>
+        <div>
+            <h1>属性绑定</h1>
+            <div v-bind:class="active">属性绑定</div>
+        </div>
+    </body>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            msg:'我是胡世豪',
+            date: new Date().toLocaleString(),
+            isTrue: true,
+            buttonHtml:'<a href="https://www.baidu.com">百度链接</a>',
+            active:'active'
+        }
+    }
+}
+</script>
